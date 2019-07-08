c0950b2b8ec10e397841822c0c4a2117972dafc3
879083421@qq.com <1021880435@qq.com>
2019-07-08 17:50:48 +0800

    增加了一个eCharts功能

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app1">
     <p>第一次修改</p>
     <img src="./assets/logo.png">
+    <p>{{a}}</p>
+    <div id="echarts" :style="{width: '326px', height: '250px'}"></div>
     <router-view/>
   </div>
 </template>
@@ -11,9 +13,36 @@ export default {
   name: 'App',
   data() {
     return {
-
-    }
+      a:"111",
+    };
   },
+  methods:{
+  drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = this.$echarts.init(document.getElementById("echarts"));
+      // 绘制图表
+       var option = {
+       xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
+    }]
+};
+
+      // 为echarts对象加载数据
+      myChart.setOption(option);
+    },
+  },
+  mounted(){
+  this.drawLine();
+  }
+
 }
 </script>
 
