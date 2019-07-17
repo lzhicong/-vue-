719dbf78d12e6a9cf4740a2dadd9e54cc21a66ea
879083421@qq.com <1021880435@qq.com>
2019-07-17 09:05:26 +0800

    修改了App.vue里表单功能

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,20 @@ export default {
        yAxis: {
         type: 'value'
        },
+        legend: {
+             show:true,
+        orient:'horizontal',
+        data:[ {
+            name:'薇恩成长攻击力',
+            // textStyle:{
+            //     fontSize:12,
+            //     fontWeight:'bolder',
+            //     color:'#cccccc'
+            // },
+            // icon:'image://./assets/logo.png'//格式为'image://+icon文件地址'，其中image::后的//不能省略
+        },]
+
+        },
        grid: {
         left:'20%',
         // show:true,
@@ -38,6 +52,7 @@ export default {
         },
         },
        series: [{
+        name:'薇恩成长攻击力',
         data: [820, 932, 901, 934, 1290, 1330, 1320],
         type: 'line'
        }]
